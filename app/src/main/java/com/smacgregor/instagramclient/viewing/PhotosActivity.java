3f725cd6a6f5e2dccb6428f092a185d78fdcbfba
Scott MacGregor <smacgregor@zynga.com>
2016-02-03 23:53:51 -0800

    Add the ability to swipe to refresh

--- app/src/main/java/com/smacgregor/instagramclient/viewing/PhotosActivity.java
+++ app/src/main/java/com/smacgregor/instagramclient/viewing/PhotosActivity.java
@@ -1,6 +1,7 @@
 package com.smacgregor.instagramclient.viewing;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -21,7 +22,7 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import cz.msebera.android.httpclient.Header;
 
-public class PhotosActivity extends AppCompatActivity {
+public class PhotosActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     private final String INSTAGRAM_CLIENTID = "e05c462ebd86446ea48a5af73769b602";
 
@@ -29,6 +30,7 @@ public class PhotosActivity extends AppCompatActivity {
     private InstagramPhotosAdapter photosAdapter;
 
     @Bind(R.id.listViewPhotos) ListView listViewPhotos;
+    @Bind(R.id.swipeContainer) SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +41,8 @@ public class PhotosActivity extends AppCompatActivity {
         photos = new ArrayList<>();
         photosAdapter = new InstagramPhotosAdapter(this, photos);
         listViewPhotos.setAdapter(photosAdapter);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setNestedScrollingEnabled(true);
 
         // send out an api request to popular photos
         fetchPopularPhotos();
@@ -49,24 +53,31 @@ public class PhotosActivity extends AppCompatActivity {
         RequestParams params = new RequestParams();
         params.put("key", "value");
         params.put("more", "data"); https://api.instagram.com/v1/tags/search?q=snowy&
+        swipeRefreshLayout.setRefreshing(true);
         client.get("https://api.instagram.com/v1/tags/bentleypup/media/recent/?client_id=" + INSTAGRAM_CLIENTID, null, new TextHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, String response) {
                 Log.i("DEBUG", response.toString());
-
+                photos.clear();
                 photos.addAll(InstagramPhotosResponse.parseJSON(response).photos);
                 photosAdapter.notifyDataSetChanged();
+                swipeRefreshLayout.setRefreshing(false);
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
                 // Do something useful
+                swipeRefreshLayout.setRefreshing(false);
             }
         });
     }
 
+    @Override
+    public void onRefresh() {
+        fetchPopularPhotos();
+    }
 
-        @Override
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_photos, menu);
